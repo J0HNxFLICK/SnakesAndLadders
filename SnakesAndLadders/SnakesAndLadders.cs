ed65807ccefc597b441282105e98b48c71c5bc59
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-25 19:05:07 +0530

    [Arun] Added a single player with starting position as 0.

--- SnakesAndLadders/SnakesAndLadders.cs
+++ SnakesAndLadders/SnakesAndLadders.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snakes and Ladders program!");
+            const int maxTileNumber = 100;
+            int playerPosition = 0; // UC1 Snake and ladder game played with single player at start position 0
+
+            Console.WriteLine("Enter player name : ");
+            string playerName = Console.ReadLine();
 
         }
     }
