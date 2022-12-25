140c9f9f25f71de085b96ae040af783064fa1a12
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-25 19:42:12 +0530

    [Arun] Added 3 conditions of play No Play / Ladder / Snake.

--- SnakesAndLadders/SnakesAndLadders.cs
+++ SnakesAndLadders/SnakesAndLadders.cs
@@ -7,14 +7,37 @@
             Console.WriteLine("Welcome to Snakes and Ladders program!");
             Random random = new Random();
             const int maxTileNumber = 100;
+            int dice = 0;
             int playerPosition = 0; // UC1 Snake and ladder game played with single player at start position 0
 
             Console.WriteLine("Enter player name : ");
-            string playerName = Console.ReadLine();
+            string playerOneName = Console.ReadLine();
 
             while (playerPosition <= maxTileNumber)
             {
-                playerPosition += random.Next(1,7); // UC2 The player rolls a die to get a number between 1 to 6.
+                int decider = random.Next(0, 3); //UC3 Player checks for No play, Ladder or Snake
+                switch (decider)
+                {
+                    case 0:
+                        Console.WriteLine(playerOneName + " got no play.\n");
+                        break;
+
+                    case 1:
+                        Console.WriteLine("Great! " + playerOneName + " found a ladder.");
+                        dice = random.Next(1, 7); // UC2 The player rolls a die to get a number between 1 to 6.
+                        playerPosition += dice;
+                        Console.WriteLine("{0} is currently at {1}\n" , playerOneName , playerPosition);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("SSSSS! " + playerOneName + " has landed on a snake !");
+                        dice = random.Next(1, 7); // UC2 The player rolls a die to get a number between 1 to 6.
+                        playerPosition -= dice;
+                        Console.WriteLine("{0} is currently at {1}\n", playerOneName, playerPosition);
+                        break;
+
+                }
+
             }
 
         }
