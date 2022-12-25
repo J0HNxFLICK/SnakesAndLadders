1cc51b058a883b24e402c6e128d3049f50879b98
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-25 19:20:05 +0530

    [Arun] Added RANDOM to roll a die.

--- SnakesAndLadders/SnakesAndLadders.cs
+++ SnakesAndLadders/SnakesAndLadders.cs
@@ -5,11 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snakes and Ladders program!");
+            Random random = new Random();
             const int maxTileNumber = 100;
             int playerPosition = 0; // UC1 Snake and ladder game played with single player at start position 0
 
             Console.WriteLine("Enter player name : ");
             string playerName = Console.ReadLine();
+
+            while (playerPosition <= maxTileNumber)
+            {
+                playerPosition += random.Next(1,7); // UC2 The player rolls a die to get a number between 1 to 6.
+            }
 
         }
     }
